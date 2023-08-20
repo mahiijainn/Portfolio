5371057ba3739452723916fd22417010f92c3484
Mahi <mahijain8955@gmail.com>
2023-08-20 10:43:40 +0530

    made navbar more adaptle to viewport changes

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// toggle
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick= () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
@@ -13,9 +22,12 @@ window.onscroll = () => {
                 link.classList.remove('active');
             });
 
-            document.querySelector('header nav a[href*="' + id + '"]').classList.add("active");
+            document.querySelector('header nav a[href*=' + id + ']').classList.add("active");
         }
     });
+    // sticky navbar
+    let header = document. querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
 };
 
 ScrollReveal({
